3774ff764bf2dab9e82a27291444a14e8e40dd8d
Dirk Arnez <smalldirkalex@gmail.com>
2022-08-22 02:20:20 +0800

    - update local-build.cmd

--- local-build.cmd
+++ local-build.cmd
@@ -1,14 +1,25 @@
 REM run as Administrator
 @echo off
+
 cd /d %~dp0
-set DOWNLOAD_DIR=%USERPROFILE%\Downloads
-set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-win32;%DOWNLOAD_DIR%\mingw64-win32\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;%PATH%
+@REM set DOWNLOAD_DIR=%USERPROFILE%\Downloads
+@REM set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
+@REM SET PATH=^
+@REM %DOWNLOAD_DIR%\PortableGit\bin;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0;^
+@REM %DOWNLOAD_DIR%\x86_64-8.1.0-release-win32-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;^
 
-cmake.exe -G"MinGW Makefiles" -DYAML_CPP_ROOT="%DOWNLOAD_DIR_LINUX%/yaml-cpp-v0.7.0-mingw/share/cmake/yaml-cpp" -B./build
-cd build
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
-:rebuild_and_startapp
-mingw32-make.exe
+cmake.exe -G"MinGW Makefiles" ^
+-DYAML_CPP_ROOT="%DOWNLOAD_DIR_LINUX%/yaml-cpp-v0.7.0-mingw/share/cmake/yaml-cpp" ^
+-DCMAKE_BUILD_TYPE=Debug ^
+-B./build &&^
+cd build &&^
+cmake --build . &&^
+echo "Successful build"
 pause
-REM GOTO rebuild_and_startapp
